b7f633055b421510ce1413a8727ed053e0ed3c69
Nick White <git@njw.name>
2021-01-19 16:54:04 +0000

    Split css into separate file, and update mkstandalone.sh accordingly

--- mkstandalone.sh
+++ mkstandalone.sh
@@ -5,6 +5,7 @@ d=`dirname "$0"`
 in="$d/proofreader.html"
 out="$d/proofreader-standalone.html"
 js="$d/proofreader.js"
+css="$d/proofreader.css"
 fn="$d/junicode.woff"
 fnb64="${fn}.base64"
 
@@ -21,12 +22,12 @@ printf '<script type="text/javascript">\n'
 cat "$js"
 printf '</script>\n'
 
-# embed font
-
-#sed -n '1,/<style>/p' < "$in" # not needed as embed js fills this part out
+# embed font and css
 printf '<style>\n'
 printf '    @font-face { font-family: "Junicode"; src: url(data:application/x-font-woff;charset=utf-8;base64,'
 cat "$fnb64"
 printf ') format("woff"); }\n'
-sed -n '/body { /,$p' < "$in"
+sed -n '2,$p' < "$css"
+printf '</style>\n'
+sed -n '/<\/head>/,$p' < "$in"
 ) > "$out"
